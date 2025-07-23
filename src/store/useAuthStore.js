701134efce1b3bd2ed17f46848c4defdcb9668b5
hieuvn8 <vungochieu2003hp@gmail.com>
2025-07-23 15:29:31 +0700

    Profile Page

--- src/store/useAuthStore.js
+++ src/store/useAuthStore.js
@@ -59,4 +59,18 @@ export const useAuthStore = create((set) => ({
       set({ isLoggingIn: false });
     }
   },
+
+  updateProfile: async (data) => {
+    set({ isUpdatingProfile: true });
+    try {
+      const res = await axiosInstance.put("/auth/updateProfile", data);
+      set({ authUser: res.data });
+      toast.success("Profile updated successfully");
+    } catch (error) {
+      console.log("error in update profile:", error);
+      toast.error(error.response.data.message);
+    } finally {
+      set({ isUpdatingProfile: false });
+    }
+  },
 }));
